95a99ca05899deaff80cad63da5f3e1081c50f7a
tsanduongvi123 <duongvitsan123@gmail.com>
2025-08-10 17:57:32 +0700

    Add files via upload

---
+++ keyviptl.py
@@ -0,0 +1,17 @@
+[
+  {
+    "key": "DGVI112010",
+    "ip_address": "192.168.1.100",
+    "expiration_date": "2025-12-31"
+  },
+  {
+    "key": "DGVIKAKA2010",
+    "ip_address": "192.168.1.101",
+    "expiration_date": "2026-01-15"
+  },
+  {
+    "key": "DGVI-VN",
+    "ip_address": "192.168.1.102",
+    "expiration_date": "2026-02-28"
+  }
+]
