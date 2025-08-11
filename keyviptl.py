08047e83e522dad6b8e5f8168db1ae5eed330cc8
tsanduongvi123 <duongvitsan123@gmail.com>
2025-08-11 15:56:59 +0700

    Update keyviptl.py

--- keyviptl.py
+++ keyviptl.py
@@ -1,20 +1,17 @@
 [
   {
     "key": "DGVI112010",
-    "ip_address": "14.191.72.172",
+    "user_id": "6415297481",
     "expiration_date": "2025-12-31"
   },
   {
     "key": "DGVIKAKA2010",
-    "ip_address": "14.191.72.172",
+    "user_id": "6415297481",
     "expiration_date": "2026-01-15"
   },
   {
     "key": "DGVI-VN",
-    "ip_address": "14.191.72.172",
+    "user_id": "6415297481",
     "expiration_date": "2026-02-28"
   }
-
 ]
-
-
