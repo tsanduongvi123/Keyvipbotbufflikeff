3358a276c464c5ed61337c6ffeb244295fe7decb
tsanduongvi123 <duongvitsan123@gmail.com>
2025-08-14 10:55:54 +0700

    Update keyviptl.py

--- keyviptl.py
+++ keyviptl.py
@@ -2,7 +2,7 @@
   {
     "key": "DGVI112010",
     "user_id": "6237327806",
-    "expiration_date": "2025-8-13"
+    "expiration_date": "9999-9-9"
   },
   {
     "key": "DGVIKAKA2010",
@@ -12,14 +12,15 @@
   {
     "key": "DGVIKAKA1111",
     "user_id": "6039814076",
-    "expiration_date": "2030-01-15"
+    "expiration_date": "9999-9-9"
   },
   {
     "key": "DGVI-VN",
     "user_id": "6415297481",
-    "expiration_date": "2025-08-13"
+    "expiration_date": "9999-9-9"
   }
 ]
+
 
 
 
